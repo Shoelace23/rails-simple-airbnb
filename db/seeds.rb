5c01e076fed2acc0728f25f193bb34effd37fd09
Francois Bracq <francois.bracq@gmail.com>
2026-05-20 12:36:10 +0200

    airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,37 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200'
+)
+
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'Lovely warm comfortable and stylishly furnished house. Private bedroom and bathroom with shared living areas.',
+  price_per_night: 65,
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1518780664697-55e3ad937233?w=1200'
+)
+
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  description: 'A mini apartment within the clock tower apartment at St Pancras Station, with its own en suite bathroom and kitchen and sitting area.',
+  price_per_night: 110,
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?w=1200'
+)
+
+Flat.create!(
+  name: 'A Little Retro in Paris',
+  address: '10 Rue de la Paix Paris',
+  description: 'A lovely retro feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 85,
+  number_of_guests: 4,
+  picture_url: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb?w=1200'
+)
